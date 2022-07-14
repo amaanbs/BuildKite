6dd07c963009aca781871d80c55d824588bef273
amaanbs <amaan.h@browserstack.com>
2022-07-14 20:08:48 +0530

    first

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+group :test do
+  gem 'rspec', '~>3.7'
+  gem "buildkite-test_collector"
+end
---
+++ spec/helper_spec.rb
@@ -0,0 +1,9 @@
+describe "customer" do
+  it "puts the ordered book in customer's order history" do
+    expect(true).to eql(true)
+  end
+
+  it "puts the ordered book in customer's order history" do
+    expect(['1','2']).to include('1')
+  end
+end
